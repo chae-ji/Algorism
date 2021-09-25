7e2b17e2ac2ba24b363f347697ff55b16e03e14a
chae-ji <83530520+chae-ji@users.noreply.github.com>
2021-09-25 19:59:05 +0900

    11720

--- Test.java
+++ Test.java
@@ -1,12 +1,20 @@
 package hello.core;
 
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) throws IOException {
-        int a = System.in.read();
-        System.out.println(a);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        String str = sc.next();
+        int[] num = new int[t];
+        int sum = 0;
 
+        for(int i = 0; i < t; i++){
+            num[i] = str.charAt(i) - 48;
+            sum += num[i];
+        }
+        System.out.println(sum);
     }
 }
 
